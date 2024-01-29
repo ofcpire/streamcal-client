6ce5c0d43c3c5fd2cc4204852ba034b361c3e60b
ofcpire <ofcpire@gmail.com>
2024-01-30 01:45:19 +0900

    Feat: 자주 사용하는 타입 선언

---
+++ src/types/index.d.ts
@@ -0,0 +1,22 @@
+interface ChannelInfoType {
+  channelId: string;
+  channelName: string;
+  verifiedMark: boolean;
+  channelType: string;
+}
+
+interface StreamcalType {
+  channelInfo: ChannelInfoType;
+  log: StreamLogType[];
+}
+
+interface StreamLogType {
+  timestamp: string;
+  _id: string;
+  channelId: string;
+  liveTitle: string;
+  __v: string;
+  liveCategory: string;
+  status: string;
+  liveCategoryValue: string;
+}
