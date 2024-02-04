2307328a43f30ddb6c98646a99a1d872eee30128
ofcpire <ofcpire@gmail.com>
2024-02-05 01:18:50 +0900

    Feat: 헤더 클릭시 메인으로 이동

--- src/components/global/Header.tsx
+++ src/components/global/Header.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const nav = useNavigate();
+  const goToListHandler = () => {
+    nav('');
+  };
+
   return (
     <header>
-      <h1>STREAMCAL</h1>
+      <h1>
+        <button onClick={goToListHandler}>STREAMCAL</button>
+      </h1>
     </header>
   );
 }
