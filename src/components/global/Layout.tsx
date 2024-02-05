7d6b3c8f215d21e59f44f3f25683fe6869088da0
ofcpire <ofcpire@gmail.com>
2024-02-06 04:43:49 +0900

    Feat: 페이지 이동 시 오토스크롤

--- src/components/global/Layout.tsx
+++ src/components/global/Layout.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './Header';
 import Footer from './Footer';
+import ScrollToTop from './ScrollToTop';
 
 function Layout() {
   return (
     <>
-      <Header />
-      <main className='font-sans antialiased bg-white flex md:max-w min-h-[95vh] justify-center pt-[64px] md:pt-[88px]'>
-        <Outlet />
-      </main>
-      <Footer />
+      <ScrollToTop>
+        <Header />
+        <main className='font-sans antialiased bg-white flex md:max-w min-h-[95vh] justify-center pt-[64px] md:pt-[88px]'>
+          <Outlet />
+        </main>
+        <Footer />
+      </ScrollToTop>
     </>
   );
 }
