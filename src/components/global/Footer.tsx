e2852fc495d87dea18d0c01804a1f94ff94eb6ca
ofcpire <ofcpire@gmail.com>
2024-02-06 05:17:43 +0900

    Fix: footer 문구 수정

--- src/components/global/Footer.tsx
+++ src/components/global/Footer.tsx
@@ -3,13 +3,16 @@ import { useNavigate } from 'react-router-dom';
 
 export default function Footer() {
   const nav = useNavigate();
-  const footerTempHandler = () => {
+  const footerNavHandler = () => {
     nav('');
   };
 
   return (
-    <footer className='p-4  md:p-6 text-center md:text-left bg-brightDarkColor text-white'>
-      2024 STREAMCALⓒ
+    <footer className='p-4 md:p-6 text-center md:text-left bg-brightDarkColor text-white'>
+      <span className='block m-2'>2024 STREAMCAL ⓒ</span>
+      <span className='block m-2'>
+        STREAMCAL은 수익을 창출하지 않습니다.
+      </span>
     </footer>
   );
 }
