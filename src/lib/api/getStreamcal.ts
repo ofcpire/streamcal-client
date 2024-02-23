c6ac006661000abd2701af693b4dc93f2233a29e
ofcpire <ofcpire@gmail.com>
2024-02-24 01:58:09 +0900

    Fix: api 함수에서 로그 정렬

--- src/lib/api/getStreamcal.ts
+++ src/lib/api/getStreamcal.ts
@@ -1,12 +1,15 @@
 import api from './axios';
+import logSorter from '../utils/streamcal/logSorter';
 
 const getStreamcal = async (
   channelId: string,
-  date?: string
+  date?: string,
+  type = 'date'
 ): Promise<StreamcalType> => {
-  const params = date ? { date } : {};
+  const params = { date, type };
   try {
     const streamLog = await api.get(`streamcal/${channelId}`, { params });
+    streamLog.data.log = logSorter(streamLog.data.log);
     return streamLog.data;
   } catch (err) {
     console.log(err);
