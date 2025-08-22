a87f00d0c6505a23e6a8d24373ac1e2876ccd946
xvive-shl <shl_developer@xvive.xyz>
2025-08-23 02:48:39 +0900

    Fix: 띄어쓰기 누락으로 인한 스타일 오류 수정 및 prettier 설정 변경

--- src/components/streamcalPage/Monthly/MonthLogViewerItem.tsx
+++ src/components/streamcalPage/Monthly/MonthLogViewerItem.tsx
@@ -31,30 +31,35 @@ export default function MonthLogViewerItem({
     )
   );
   return (
-    <div className='flex sm:flex-col sm:min-h-[100px] rounded bg-white dark:bg-scDarkColor m-1 '>
+    <div className="flex sm:flex-col sm:min-h-[100px] rounded bg-white dark:bg-scDarkColor m-1 ">
       <span
         className={
-          'block border-r sm:border-b sm:border-r-0 dark:border-scBrightDarkColor text-center rounded-l sm:rounded-t sm:rounded-bl-none text-sm font-semibold p-2 sm:p-0 min-w-[40px]' +
+          'block border-r sm:border-b sm:border-r-0 dark:border-scBrightDarkColor text-center rounded-l' +
+          ' ' +
+          'sm:rounded-t sm:rounded-bl-none text-sm font-semibold p-2 sm:p-0 min-w-[40px]' +
+          ' ' +
           (dateLog.type === 's'
             ? 'text-white bg-scBrightRedColor sm:text-scBrightRedColor sm:bg-white dark:bg-scDarkColor'
             : 'text-scBrightDarkColor dark:text-scOffWhiteColor bg-white dark:bg-scDarkColor')
-        }>
+        }
+      >
         {dateLog.date}
       </span>
-      <div className='flex sm:flex-col m-1 flex-wrap items-center sm:items-start'>
+      <div className="flex sm:flex-col m-1 flex-wrap items-center sm:items-start">
         {dateLog.key === dayjs(createdAt).format('YYYYMMDD') ? (
-          <span className='sc-smInfoBox m-0 mr-1 sm:mr-0'>기록 시작</span>
+          <span className="sc-smInfoBox m-0 mr-1 sm:mr-0">기록 시작</span>
         ) : dateLog.key === dayjs(today).format('YYYYMMDD') ? (
-          <span className='sc-smInfoBox m-0 mr-1 sm:mr-0'>오늘</span>
+          <span className="sc-smInfoBox m-0 mr-1 sm:mr-0">오늘</span>
         ) : null}
         {categories.map((log, i) => {
           return (
             <span
-              className='inline-block text-xs text-wrap pr-1 sm:pr-0'
-              key={log}>
+              className="inline-block text-xs text-wrap pr-1 sm:pr-0"
+              key={log}
+            >
               {log}
               {i !== categories.length - 1 ? (
-                <span className='sm:hidden'>,</span>
+                <span className="sm:hidden">,</span>
               ) : null}
             </span>
           );
